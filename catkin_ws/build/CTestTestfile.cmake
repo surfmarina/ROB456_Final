440d610dab2f5945a58c02c28ca9476f0b0e4bd9
Taylor <amarotta@oregonstate.edu>
2021-12-08 20:18:23 +0000

    add catkin_ws folder

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/ROB456_Final/catkin_ws/src
+# Build directory: /home/ubuntu/ROB456_Final/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
