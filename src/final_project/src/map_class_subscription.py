72bd6bd78136ffe53a535f80f5376ce24176cd01
C. Greyston Brady <c.greyston.brady@gmail.com>
2021-12-09 20:44:24 -0800

    Temp removed A*

--- src/final_project/src/map_class_subscription.py
+++ src/final_project/src/map_class_subscription.py
@@ -26,49 +26,20 @@ class GlobalPlannar(object):
 
         data = np.asarray(msg.data, dtype=np.int8).reshape(msg.info.height, msg.info.width)
 
+        # a* begin, move to seperate node
+
         a = aS.aStar([100, 100], [250, 325], img=data)
-        aPath = a.path(a.run())
 
-        rospy.loginfo(np.shape(aPath))
+        # aPath = a.path(a.run())
 
-        # Astar goes here
+        # rospy.loginfo(np.shape(aPath))
+
 
         self.pub.publish(True)
 
     def odom_callback(self, msg):
         # rospy.loginfo('In odom callback' + str(self.odom_pos))
         self.odom_pos = msg.pose.pose
-
-def draw_points(points, pub):
-    """
-    Plots an array of points [(x, y)...] in rviz
-
-    :param: points iterable of (x, y) pairs. If a numpy array, shape should be (n, 2)
-    :return: None
-    """
-    msg = Marker()
-    # Marker header specifies what (and when) it is drawn relative to
-    msg.header.frame_id = "map"
-    msg.header.stamp = rospy.Time.now()
-    # uint8 POINTS=8
-    msg.type = 8
-    # Action, Create/Update
-    msg.action = 0
-    # Disappear after 1sec. Comment this line out to make them persist indefinitely
-    msg.lifetime = rospy.rostime.Duration(1, 0)
-    # Set marker visual properties
-    msg.color.b = 1.0
-    msg.color.a = 1.0
-    msg.scale.x = 0.2
-    msg.scale.y = 0.2
-    # Copy  (x, y) into message and publish
-    for (x, y) in points:
-        p = Point()
-        p.x = x
-        p.y = y
-        p.z = 0.1 # Places all points 10cm above the ground
-        msg.points.append(p)
-    pub.publish(msg)
 
 if __name__ == '__main__':
     rospy.init_node('class_example')
